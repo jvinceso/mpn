554914b25827ebead6b72001ef41c27d87cb9d7d
Jose Vinces <jvinceso@gmail.com>
2014-03-22 11:42:16 -0500

    ignorando configuraciones

--- application/logs/log-2014-03-22.php
+++ application/logs/log-2014-03-22.php
@@ -476,3 +476,35 @@ DEBUG - 2014-03-22 17:13:55 --> Model Class Initialized
 DEBUG - 2014-03-22 17:13:55 --> Table Class Initialized
 DEBUG - 2014-03-22 17:13:56 --> Final output sent to browser
 DEBUG - 2014-03-22 17:13:56 --> Total execution time: 0.1054
+DEBUG - 2014-03-22 17:19:41 --> Config Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Hooks Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Utf8 Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> UTF-8 Support Enabled
+DEBUG - 2014-03-22 17:19:41 --> URI Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Router Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Output Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Security Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Input Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-03-22 17:19:41 --> Language Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Loader Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Helper loaded: url_helper
+DEBUG - 2014-03-22 17:19:41 --> Helper loaded: form_helper
+DEBUG - 2014-03-22 17:19:41 --> Helper loaded: webservices_helper
+DEBUG - 2014-03-22 17:19:41 --> Database Driver Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Session Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Helper loaded: string_helper
+DEBUG - 2014-03-22 17:19:41 --> Session routines successfully run
+DEBUG - 2014-03-22 17:19:41 --> Controller Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Model Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Model Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Table Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> Helper loaded: tables_helper
+DEBUG - 2014-03-22 17:19:41 --> Model Class Initialized
+DEBUG - 2014-03-22 17:19:41 --> File loaded: application/views/plantilla/header.php
+DEBUG - 2014-03-22 17:19:41 --> File loaded: application/views/utilitarios/modulo/ins_view.php
+DEBUG - 2014-03-22 17:19:41 --> File loaded: application/views/utilitarios/modulo/panel_view.php
+DEBUG - 2014-03-22 17:19:41 --> File loaded: application/views/plantilla/footer.php
+DEBUG - 2014-03-22 17:19:41 --> File loaded: application/views/plantilla/template.php
+DEBUG - 2014-03-22 17:19:41 --> Final output sent to browser
+DEBUG - 2014-03-22 17:19:41 --> Total execution time: 0.0612
