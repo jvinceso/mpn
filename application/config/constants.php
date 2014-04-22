9058d1a5a2bf8663b43f7c0ea9b6e65be5c1b692
Yamasaky <diegokyo216@hotmail.com>
2014-04-21 19:17:40 -0500

    fr

--- application/config/constants.php
+++ application/config/constants.php
@@ -18,10 +18,7 @@ define('FILE_WRITE_MODE', 0666);
 define('DIR_READ_MODE', 0755);
 define('DIR_WRITE_MODE', 0777);
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 define('SERVER_AP', 'localhost:8081');
 define('SERVER_DB', '172.20.17.10');
 
