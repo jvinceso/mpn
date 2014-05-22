79d2c5ed9a3d5734a1e783d23489bc3e3cd27f9f
Yamasaky <diegokyo216@hotmail.com>
2014-05-21 19:55:47 -0500

    soluciono servicios costo
    rwerwer

--- application/models/agua/servicios_contribuyente_model.php
+++ application/models/agua/servicios_contribuyente_model.php
@@ -55,7 +55,7 @@
 			inner join servicios_tipo st on st.nSetId = sc.nSetId
 			inner join multitabla m on m.nMulId = st.nMulServicio
 			inner join multitabla mt on mt.nMulId = st.nMulTipoServicio
-		where sc.nSetId = '".$this->nSetId."' and nPerId = '".$this->nPerId."' and nDicId = '".$this->nDicId."'  and  sc.cSecEstado = 1");
+		where sc.nSetId = '".$this->nSetId."' and nPerId = '".$this->nPerId."' and nDicId = '".$this->nDicId."'  and  sc.cSecEstado = 1 and m.cMulEstado = 1 and mt.cMulEstado = 1" );
 		;	
 		return $query->num_rows();
 		}
